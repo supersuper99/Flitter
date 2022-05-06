09b88ca625558715132455e463510280ca9b0818
Eric <eandrews100@gmail.com>
2022-05-05 19:26:59 -0500

    initial commit

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Sidebar.css'
+import EggIcon from '@mui/icons-material/Egg';
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <EggIcon></EggIcon>
+            <h1>HELOO</h1>
+        </div>
+    )
+}
+
+export default Sidebar;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,15 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import Sidebar from '../components/Sidebar';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
+      <div className="app">
+        <h1>FLITTER</h1>
+        <Sidebar />
+      </div>
     </IonPage>
   );
 };
